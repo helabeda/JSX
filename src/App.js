a657cc0a94c06223bf81fb8ec26cd88cdca9edce
Hela <abadahela@gmail.com>
2021-07-15 00:51:31 +0100

    JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import imageInSrc from "./imageInSrc.jpg";
+import "./style.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ border: "solid 1px red", maxWidth: "100vw" }}>
+      <h1 className="title red">
+        Hela ABEDA
+      </h1>
+      <br />
+      <img width={300} height={240} src= {imageInSrc} alt="imageInSrc"/>
+      <br />
+       <img width={300} height={240}  src="/imageInPublic.jpg" alt="imageInPublic"/>
+    </div>
+      <video width={320} height={240} controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
